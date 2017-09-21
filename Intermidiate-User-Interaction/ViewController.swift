737ea21e4ff5c1e6d8e2c4bfe42cbf7452eb13db
Robert Argume <robert@roberts-macbook-pro.local>
2017-09-20 20:27:19 -0400

    Added Outlets and Actions

--- Intermidiate-User-Interaction/ViewController.swift
+++ Intermidiate-User-Interaction/ViewController.swift
@@ -2,18 +2,35 @@
  * Name: Robert Argume
  * StudentID:
  * Description: Intermediate User Interactions Demo
- * Version: 0.1 - Built Basic UI
+ * Version: 0.2 - Added Outlets and Actions
  */
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    // OUTLETS
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    // INHERITED METHODS
     override func viewDidLoad() {
         super.viewDidLoad()
       
     }
+    
+    //ACTION METHODS
+    @IBAction func onSliderChange(_ sender: UISlider) {
+    }
 
+    @IBAction func onSegmentedControlChanged(_ sender: UISegmentedControl) {
+    }
 
+    @IBAction func onDoSomethingPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func onSwitchChanged(_ sender: UISwitch) {
+    }
+    
 }
 
